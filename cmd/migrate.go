b9e83b320a07832c5f9acfd677e93dde968eef11
Osama Adam <osamaadam98@gmail.com>
2022-01-03 18:52:09 +0200

    changed `update` subcommand into `migrate` which is more fitting

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -15,18 +15,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var updateCmd = &cobra.Command{
-	Use:     "update [access.mdb path]",
+var migrateCmd = &cobra.Command{
+	Use:     "migrate [access.mdb path]",
 	Args:    cobra.NoArgs,
-	Aliases: []string{"u", "insert", "update-manzoma"},
-	Short:   "Update manzoma with the data in the access file provided",
-	RunE:    updateRun,
+	Aliases: []string{"m", "mig", "update-manzoma"},
+	Short:   "Migrate soldiers from Access to Manzoma",
+	RunE:    migrateRun,
 	Example: strings.TrimSpace(`
-		gomanz update --host http://remote-pc -u sa -p password1 --port 1433
+		gomanz migrate --host http://remote-pc -u sa -p password1 --port 1433
 	`),
 }
 
-type UpdateCmdOpts struct {
+type MigrateOpts struct {
 	Username      string
 	Password      string
 	Host          string
@@ -35,13 +35,13 @@ type UpdateCmdOpts struct {
 	Marhla        uint
 }
 
-var updateCmdOpts UpdateCmdOpts
+var migrateOpts MigrateOpts
 
-func updateRun(cmd *cobra.Command, args []string) error {
+func migrateRun(cmd *cobra.Command, args []string) error {
 	manzConnection := url.URL{
 		Scheme: "sqlserver",
-		User:   url.UserPassword(updateCmdOpts.Username, updateCmdOpts.Password),
-		Host:   fmt.Sprintf("%s:%d", updateCmdOpts.Host, updateCmdOpts.Port),
+		User:   url.UserPassword(migrateOpts.Username, migrateOpts.Password),
+		Host:   fmt.Sprintf("%s:%d", migrateOpts.Host, migrateOpts.Port),
 	}
 
 	log.Println("creating a connection to Manzoma")
@@ -57,9 +57,9 @@ func updateRun(cmd *cobra.Command, args []string) error {
 		return errors.WithStack(err)
 	}
 
-	dataSrc := filepath.Clean(updateCmdOpts.AccessSrcPath)
+	dataSrc := filepath.Clean(migrateOpts.AccessSrcPath)
 
-	log.Printf("Creating a connection to %s\n", dataSrc)
+	log.Printf("creating a connection to %s\n", dataSrc)
 	a, err := access.CreateConnection("Microsoft.ACE.OLEDB.12.0", dataSrc)
 	if err != nil {
 		return errors.WithStack(err)
@@ -80,7 +80,7 @@ func updateRun(cmd *cobra.Command, args []string) error {
 	`
 
 	log.Println("querying the access database for unregistered soldiers")
-	rows, err := a.DB.Query(sqlQuery, lastSeglNo, updateCmdOpts.Marhla)
+	rows, err := a.DB.Query(sqlQuery, lastSeglNo, migrateOpts.Marhla)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -141,16 +141,16 @@ func guessMarhla() (uint, error) {
 
 func init() {
 	curMarhla, _ := guessMarhla()
-	updateCmd.Flags().StringVarP(&updateCmdOpts.AccessSrcPath, "access", "a", "", "path to the .mdb file")
-	updateCmd.Flags().StringVar(&updateCmdOpts.Host, "host", "localhost", "host of the mssql server")
-	updateCmd.Flags().UintVar(&updateCmdOpts.Port, "port", 1433, "port of the running mssql database server")
-	updateCmd.Flags().StringVarP(&updateCmdOpts.Username, "user", "u", "", "username for the mssql database")
-	updateCmd.Flags().StringVarP(&updateCmdOpts.Password, "pass", "p", "", "password for the mssql database")
-	updateCmd.Flags().UintVarP(&updateCmdOpts.Marhla, "marhla", "m", curMarhla, "the marhla to migrate")
+	migrateCmd.Flags().StringVarP(&migrateOpts.AccessSrcPath, "access", "a", "", "path to the .mdb file")
+	migrateCmd.Flags().StringVar(&migrateOpts.Host, "host", "localhost", "host of the mssql server")
+	migrateCmd.Flags().UintVar(&migrateOpts.Port, "port", 1433, "port of the running mssql database server")
+	migrateCmd.Flags().StringVarP(&migrateOpts.Username, "user", "u", "", "username for the mssql database")
+	migrateCmd.Flags().StringVarP(&migrateOpts.Password, "pass", "p", "", "password for the mssql database")
+	migrateCmd.Flags().UintVarP(&migrateOpts.Marhla, "marhla", "m", curMarhla, "the marhla to migrate")
 
-	updateCmd.MarkFlagRequired("access")
-	updateCmd.MarkFlagRequired("user")
-	updateCmd.MarkFlagRequired("pass")
+	migrateCmd.MarkFlagRequired("access")
+	migrateCmd.MarkFlagRequired("user")
+	migrateCmd.MarkFlagRequired("pass")
 
-	rootCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(migrateCmd)
 }
